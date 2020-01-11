d24ad52045b7c05f87259a0b2eb39085d1974391
Anthony G Scire <aaaantoine@gmail.com>
2020-01-11 13:31:21 -0500

    Add D12.

--- diceroll/src/Diceroll.js
+++ diceroll/src/Diceroll.js
@@ -73,6 +73,7 @@ export default class Diceroll extends React.Component {
                     case 6: return "d6";
                     case 8: return "d8";
                     case 10: return "d10";
+                    case 12: return "d12";
                     default:
                         return null;
                 }
