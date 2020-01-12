7c47655988a9a4aa918cd5941eb3453aa0ca5ff2
Anthony G Scire <aaaantoine@gmail.com>
2020-01-11 20:16:33 -0500

    Add some basic tests for Formula module.

---
+++ src/__test__/Formula.test.js
@@ -0,0 +1,15 @@
+import Formula from '../Formula';
+
+function testTotal(expression, expected) {
+    const response = Formula.calculate(expression);
+    expect(response.total).toBe(expected);
+}
+
+test('calculates 1+1 and returns total of 2', () => testTotal("1+1", 2));
+test('calculates 8*4 and returns total of 32', () => testTotal("8*4", 32));
+test('calculates 1d1+1 and returns total of 2', () => testTotal("1d1+1", 2));
+test('calculates 4+3*2 and returns total of 10', () => testTotal("4+3*2", 10));
+test('calculates (4+3)*2 and returns total of 14', () => testTotal("(4+3)*2", 14));
+test('throws exception when calculating abc', () => {
+    expect(() => Formula.calculate("abc")).toThrow(new Error('Formula is invalid.'));
+});
