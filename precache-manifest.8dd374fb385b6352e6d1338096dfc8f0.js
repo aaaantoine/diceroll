5eccf3672588a85e2a52b318c91eec3b8bd72efe
Anthony G Scire <aaaantoine@gmail.com>
2020-01-11 19:10:57 -0500

    Updates

---
+++ precache-manifest.8dd374fb385b6352e6d1338096dfc8f0.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "752c94d39c315c9a2ab5c3802d3c842f",
+    "url": "/diceroll/index.html"
+  },
+  {
+    "revision": "d1d99427b52b427d698e",
+    "url": "/diceroll/static/css/main.364c3b9e.chunk.css"
+  },
+  {
+    "revision": "3b677adaf7ea4b962404",
+    "url": "/diceroll/static/js/2.b7a1a2f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/diceroll/static/js/2.b7a1a2f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d1d99427b52b427d698e",
+    "url": "/diceroll/static/js/main.beda3329.chunk.js"
+  },
+  {
+    "revision": "b048d93ac89478760bd3",
+    "url": "/diceroll/static/js/runtime-main.9cabb535.js"
+  },
+  {
+    "revision": "9b56193ab2d200e8e6b3e93232201405",
+    "url": "/diceroll/static/media/coin.9b56193a.svg"
+  },
+  {
+    "revision": "82ab13af96cb5242efe100258de7ef97",
+    "url": "/diceroll/static/media/coin_1.82ab13af.svg"
+  },
+  {
+    "revision": "317de74954bb92019eca1bc67ee0af84",
+    "url": "/diceroll/static/media/coin_2.317de749.svg"
+  },
+  {
+    "revision": "494af98090c7a3bbad7c62c4a362fe93",
+    "url": "/diceroll/static/media/d10.494af980.svg"
+  },
+  {
+    "revision": "f37da410e8b81075e3435f26691a46cb",
+    "url": "/diceroll/static/media/d12.f37da410.svg"
+  },
+  {
+    "revision": "5083c56ef9bf5d53006aafbb41bdc56c",
+    "url": "/diceroll/static/media/d20.5083c56e.svg"
+  },
+  {
+    "revision": "1bdaf7722fa81125855d8df82bb9cddd",
+    "url": "/diceroll/static/media/d4_1.1bdaf772.svg"
+  },
+  {
+    "revision": "bcfb0c9c069706ec803a2d58328fc063",
+    "url": "/diceroll/static/media/d4_2.bcfb0c9c.svg"
+  },
+  {
+    "revision": "c7573e933162df7411c7025b97e8e4ff",
+    "url": "/diceroll/static/media/d4_3.c7573e93.svg"
+  },
+  {
+    "revision": "881f2279f8676ab2fb683ab65f1af82e",
+    "url": "/diceroll/static/media/d4_4.881f2279.svg"
+  },
+  {
+    "revision": "cb9cc3745de38888b3689c1ef79a7652",
+    "url": "/diceroll/static/media/d6_1.cb9cc374.svg"
+  },
+  {
+    "revision": "dcb946e34a4fd4c712014b57b31ef813",
+    "url": "/diceroll/static/media/d6_2.dcb946e3.svg"
+  },
+  {
+    "revision": "cb13a182d9e3e6035816dc54f124068f",
+    "url": "/diceroll/static/media/d6_3.cb13a182.svg"
+  },
+  {
+    "revision": "f2161dffad0137a10dfff66dc4ecf3b5",
+    "url": "/diceroll/static/media/d6_4.f2161dff.svg"
+  },
+  {
+    "revision": "eb9f654df87614f679f590d285ae3db1",
+    "url": "/diceroll/static/media/d6_5.eb9f654d.svg"
+  },
+  {
+    "revision": "8e3a4e1f055259f7db92aaa175a6e596",
+    "url": "/diceroll/static/media/d6_6.8e3a4e1f.svg"
+  },
+  {
+    "revision": "02b2265ee03ea7ae3c0155281e49febb",
+    "url": "/diceroll/static/media/d8.02b2265e.svg"
+  }
+]);
