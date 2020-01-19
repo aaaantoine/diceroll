211d4df800eab7b644dbf33b2e9aa585c0eb1fe2
Anthony G Scire <aaaantoine@gmail.com>
2020-01-19 02:22:04 -0500

    At small screen sizes show only either dice or math tools.

--- src/InputToolbox.js
+++ src/InputToolbox.js
@@ -3,18 +3,46 @@ import CalcKeys from './CalcKeys.js';
 import DiceTools from './DiceTools.js';
 
 export default class InputToolbox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.dialogs = {
+            dice: {
+                label: "Dice",
+                content: <DiceTools 
+                    onDiceAddClick={this.props.onFormulaAddRequest}
+                    onRollClick={this.props.onRollRequest} />
+            },
+            calc: {
+                label: "Math",
+                content: <CalcKeys onInput={this.props.onFormulaAddRequest} />
+            }
+        };
+        this.state = {
+            openDialog: this.dialogs.dice
+        };
+    }
     render() {
         return (
-            <div class="row justify-content-center">
-                <div class="col-lg-4">
-                    <DiceTools 
-                        onDiceAddClick={this.props.onFormulaAddRequest}
-                        onRollClick={this.props.onRollRequest} />
+            <React.Fragment>
+                <div class="row justify-content-center d-lg-none">
+                    <div class="btn-group">
+                        {Object.values(this.dialogs).map(dialog => (
+                            <button
+                                class={"btn btn-outline-secondary btn-sm" + (this.state.openDialog === dialog ? " active" : "")}
+                                onClick={() => this.setState({openDialog: dialog})}>
+                                {dialog.label}
+                            </button>
+                        ))}
+                    </div>
                 </div>
-                <div class="col-lg-4">
-                    <CalcKeys onInput={this.props.onFormulaAddRequest} />
+                <div class="row justify-content-center">
+                    {Object.values(this.dialogs).map(dialog => (
+                        <div class={"col-lg-4" + (this.state.openDialog === dialog ? "" : " d-none d-lg-block")}>
+                            {dialog.content}
+                        </div>
+                    ))}
                 </div>
-            </div>
+            </React.Fragment>
         );
     }
 }
