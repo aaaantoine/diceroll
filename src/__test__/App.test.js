c3bcb7c03ddd1797fd291284f081bf14267e2df5
Marty Powell <jpowel25@gmail.com>
2020-01-21 20:44:23 -0500

    added app testing

---
+++ src/__test__/App.test.js
@@ -0,0 +1,17 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import App from "../App";
+import userEvent from "@testing-library/user-event";
+
+test("should perform a basic dice calculation", async () => {
+  const { getByText, getByLabelText } = render(<App />);
+  const formulaInput = getByLabelText(/formula/i);
+  const submitButton = getByText(/submit/i);
+
+  await userEvent.type(formulaInput, "d2+4");
+  userEvent.click(submitButton);
+
+  // Results of submitting the form
+  getByText(/re-roll/i);
+  getByText(/6/i);
+});
